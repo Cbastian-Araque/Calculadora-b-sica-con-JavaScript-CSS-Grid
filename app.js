8ece2535b4521f69669dfa426ff17c394642c369
Sebastian <cbastian97araque@gmail.com>
2020-12-19 09:52:30 -0500

    se establecen funciones para las teclas de operación, igual y borrar

--- app.js
+++ app.js
@@ -7,4 +7,57 @@ const teclaBorrar = document.getElementsByName('borrar')[0];
 const teclaLimpiar = document.getElementsByName('limpiar')[0];
 const teclaDecimal = document.getElementsByName('decimal')[0];
 
-console.log(teclaNumero);
+let result = document.getElementById('item16');
+
+//Sera la operación actual de la calculadora
+let opeActual = '';
+
+//Operación anterior para guardarla y reutilizarla para seguir operando
+let OpeAnterior = '';
+let operacion = '';
+
+
+teclaNumero.forEach(function(boton){
+  boton.addEventListener('click', function(){
+    capturarNumero(boton.innerText);
+
+    // alert(boton.innerText); //Se usa alert, para saber si se capturan lo valores de cada tecla de numeros
+  })
+});
+
+teclaSigno.forEach(function(boton){
+  boton.addEventListener('click', function(){
+    selectOperacion(boton.innerText);
+    // alert(boton.innerText); //para depurar si funciona el evento click.
+  })
+});
+
+teclaIgual.addEventListener(function(boton){
+  calcular();
+  mostrarPantalla();
+});
+
+teclaLimpiar.addEventListener('click',function(){
+  clear();
+  mostrarPantalla();
+});
+
+// teclaDecimal.addEventListener('click' function(boton){
+//   capturarTecla(boton.innerText);
+//   alert(boton.innerText);
+// })
+
+function agregarNumero(num){
+  opeActual = opeActual.toString() + num.toString(); //se convierten los numeros en pantalla a formato texto y se concatena con mas tipos de texto
+  mostrarPantalla();
+}
+
+function clear(){
+  opeActual = '';
+  OpeAnterior = '';
+  operacion = undefined;
+}
+
+function mostrarPantalla(){
+  result.value = opeActual;
+}
